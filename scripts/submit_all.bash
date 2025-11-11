00e04b7c64e809a924d9686df17290d8a69d6fe2
seba42 <fiasebastian.04@gmail.com>
2025-11-11 22:28:22 +0100

    fix job submission (again)

--- scripts/submit_all.bash
+++ scripts/submit_all.bash
@@ -1,6 +1,21 @@
 #!/bin/bash
+JOB_WALLTIME="05:30:00"
+JOB_MEMORY="16gb"
+
 for N_THREADS in 1 2 4 8 16 32; do
-  for MTX_FILE_PATH in $MATRICES_DIR/*.mtx; do
-    qsub -v JOB_N_THREADS=$N_THREADS,JOB_MTX_FILE_PATH=$MTX_FILE_PATH job.pbs
-  done
+    for MTX_FILE_PATH in $MATRICES_DIR/*.mtx; do
+        
+        MATRIX_FILENAME=$(basename "$MTX_FILE_PATH")
+        
+        # Example: "twotone.mtx" -> "twotone"
+        MATRIX_BASE_NAME=${MATRIX_FILENAME%.*}
+        JOB_NAME="spmv_test_${N_THREADS}_${MATRIX_BASE_NAME}"
+
+        echo "Submitting job: ${MATRIX_BASE_NAME} with $N_THREADS threads"
+
+        qsub -N ${JOB_NAME} \
+             -v JOB_N_THREADS=${N_THREADS},JOB_MTX_FILE_PATH=${MTX_FILE_PATH} \
+             -l select=1:ncpus=${N_THREADS}:mem=${JOB_MEMORY},walltime=${JOB_WALLTIME} \
+             scripts/job.pbs
+    done
 done
